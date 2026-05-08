3a5f3fd8d600673555f03b125de7682799163205
SuperWolf121 <denysmail55@gmail.com>
2026-05-08 18:08:41 +0200

    d

--- guitar-chords-app/src/App.jsx
+++ guitar-chords-app/src/App.jsx
@@ -412,6 +412,13 @@ function App() {
   capo: 2,
   link: "https://tabs.ultimate-guitar.com/tab/4-non-blondes/whats-up-chords-349210",
 },
+  {
+  name: "Whats up",
+  difficulty: 1,
+  chords: "G C Am",
+  capo: 2,
+  link: "https://tabs.ultimate-guitar.com/tab/4-non-blondes/whats-up-chords-349210",
+},
     
   ]
 
